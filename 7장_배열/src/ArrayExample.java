8d7decb951ec9ae149317d47c3afadab7a640b80
chappsal <heyhighbyee@gmail.com>
2021-12-16 19:01:44 +0900

    211216

--- 7장_배열/src/ArrayExample.java
+++ 7장_배열/src/ArrayExample.java
@@ -109,6 +109,7 @@ public class ArrayExample {
 		// 12/13 과제 1
 		// A,B,C, 에서 마지막 콤마 제외하고 [A, B, C] 출력 : 반복문 for 이용 + 조건연산자(조건문)?참:거짓  또는 다른 방법들 이용
 		System.out.println("과제1");
+
 		String temp="";
 		
 		for(i=0;i<ch.length;i++) {
@@ -118,21 +119,59 @@ public class ArrayExample {
 			else {
 			temp += ch[i]+", ";
 			}
-			
-			
 		}
 		System.out.println("["+temp+"]");
+		System.out.println();
+		
+		//풀이 1 ----------------------------------------------------
+		//ch = {'A', 'B', 'C'};
+		String str="";
+		
+		
+		System.out.println("- 과제 1 : 방법 1");
+		
+		for(i = 0; i < ch.length; i++) {
+			//str += ((i == ch.length-1)? ch[i] + "" : ch[i] + ", ");
+			str += ch[i] +((i != ch.length-1)?  ", " : "");
+		}
+		System.out.println("[" + str + "]");
 		
 		
 		
+		//----------------------------------------------------
 		
-		System.out.println("과제1 끝");
+		System.out.println("- 과제 1 : 방법 2");
+		
+		str = "";
+		
+		for(i = 0; i < ch.length; i++) {
+			
+			str += ch[i] + ", ";
+		}
+		int idx = str.length();
+		str = str.substring(0, idx-2);
+		System.out.println("[" + str + "]");
+		
+
+		//-----------------------------------------------------
+		
+		System.out.println("- 과제 1 : 방법 3");
+		
+		for(i = 0; i < ch.length; i++) {
+			
+			if(i == 0) System.out.print("[" + ch[i] + ", ");
+			else if (i == ch.length-1) System.out.print(ch[i] + "]");
+			else System.out.print(ch[i] + ", ");
+		}
+		System.out.println();
+
+		
 		
 		/*
 		 * java.util.Arrays 클래스 : 배열을 조작하기 위해 만들어진 클래스
 		 */
 		
-		System.out.println(Arrays.toString(ch)); // [A, B, C] 배열 출력 시 사용 (for문보다 간편)
+		System.out.println("Arrays.toString()사용"+Arrays.toString(ch)); // [A, B, C] 배열 출력 시 사용 (for문보다 간편)
 		
 		/* 
 		 * ArrayIndexOutOfBoundsException 예외발생=예외객체 생성
@@ -193,7 +232,8 @@ public class ArrayExample {
 		// 12/13 과제 2
 		// sco1가 참조하는 값들을 0으로 변경 후 [0, 10, 20]
 		System.out.println("과제2");
-
+		
+		
 		int c;
 		for(c=0,i=1; i<sco1.length;c++) {
 			if(sco1[2]==0) {
@@ -202,10 +242,26 @@ public class ArrayExample {
 			} else
 			sco1[c]-=10;
 		} System.out.println(Arrays.toString(sco1));
+			
+		
+		System.out.println();
+		
+		//풀이 2---------------------------------------------------
+		
+		
+		System.out.println("과제 2 : 풀이 2");
+		
+		for(i=1; i<sco1.length;i++) {
+			sco1[i]=0;
+		}
+		
+		for(i = 0; i < sco1.length; i++) {
+			if( i != 0) sco1[i] = sco1[i-1] + 10;
+		}
 		
 		
 		
-
+		
 		// 12/13 과제 3
 		/*
 		 *  키보드로부터 학생 수를 입력받아 sco2가 참조하는 배열객체 생성 후
@@ -213,6 +269,7 @@ public class ArrayExample {
 		 *  합과 평균 구하기
 		 *  
 		 */
+		System.out.println("과제3");
 		
 		i=0;
 		int hap=0;
@@ -221,7 +278,7 @@ public class ArrayExample {
 		
 		int[] sco2 = null;
 		
-		System.out.println("학생 수 입력:");
+		System.out.print("학생 수 입력:");
 		i = sc.nextInt();
 		sco2 = new int[i];
 		
@@ -232,10 +289,128 @@ public class ArrayExample {
 		}
 		System.out.println("합="+hap+" 평균="+(double)hap/sco2.length);
 		
+		//풀이 2-------------------------------------------------------
+		
+		double avg;
+		
+		int[] sco3 = null;
+		System.out.print("학생 수 입력 : ");
+		
+		int stuNum = sc.nextInt();
+		sco3 = new int[stuNum];
+		
+		
+		sum=0;
+		for(i = 0; i < stuNum; i++) {
+			System.out.print((i + 1) + "번 점수 입력 : ");
+			sco3[i] = sc.nextInt();	
+			if( !(0<=sco3[i] && sco3[i]<=100) ) {
+			// if( 0 > sco3[i] && sco3[i] > 100) ){
+				System.out.print("0~100사이의 정수를 입력하시오");
+				i--;
+				continue;
+			}
+			sum += sco3[i];
+		}
 		
 		
 		
-	
+		avg = (double)sum / stuNum;
+		System.out.printf("합 = " + sum + ", 평균 = %.2f", avg);
+		
+		// 방법 2
+		// System.out.printf("합 = " + sum + ", 평균 = " + (double)sum/sco2.length);
+		
+		/*************************  배열 선언 후 배열목록 생성 방법 2   ***************************/
+		//방법1
+		int[] sco4= new int[3]; //[0,0,0] 기본값
+		
+		sco4[0] = 3; // 초기값
+		sco4[1] = 4;
+		sco4[2] = 5;
+		
+		//방법2
+		int[] sco5 = new int[] {3,4,5}; // 초기값, new int[] 생략 가능
+		//방법3
+		int[] sco6 = {3,4,5};
+		//방법4
+		int[] sco7 = null; //배열 선언
+		sco7 = new int[] {3,4,5}; // new int[] 생략 불가
+		
+		
+		/**
+		 * 향상된 for문 정리 : index를 이용하지 않음
+		 * 				   따라서 아래 방법 중 방법 2만 정상적인 결과 출력
+		 * 
+		 * 				 index로 접근하지 않기 때문에 배열 안의 값을 변경할 수 없음
+		 * 				 단지 배열 값을 참조만 할 수 있음
+		 */
+		
+		//과제 1 을 향상된 for문으로 처리
+		char[] arr = {'A', 'B', 'C'};
+		
+		
+		String str2="";
+		
+		
+		System.out.println("- 과제 1 : 방법 1");
+		
+		for(i = 0; i < arr.length; i++) {
+			
+			str2 += ((i == arr.length-1)? arr[i] + "" : arr[i] + ", ");
+		}
+		System.out.println("[" + str2 + "]");
+		
+		//------------------------------------------------------
+		/*
+		System.out.println("- 과제 1 : 향상된 for - 방법 1 사용불가");
+		for(char value : arr) {
+			str2 += ((i == arr.length-1)? value + "" : value + ", ");
+		}
+		*/
+
+		//-----------------------------------------------------
+		
+		System.out.println("- 과제 1 : 향상된 for - 방법 2 사용가능");
+		str2="";
+		for(char value: arr) {
+			
+			str2 += value + ", ";
+		}
+		str2 = str2.substring(0, str2.length()-2);
+		System.out.println("["+ str2+"]");
+		
+		//----------------------------------------------------
+		
+		System.out.println("- 과제 1 : 향상된 for - 방법 3 사용불가");
+		/*
+		for(char value : arr) {
+			
+			if(i == 0) System.out.print("[" + value + ", ");
+			else if (i != arr.length-1) System.out.print(value + "]");
+			else System.out.print(value + ", ");
+		}
+		
+		System.out.println();
+		*/
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}//main
 
 }
